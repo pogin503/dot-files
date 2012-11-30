ccf21a2a67fd16c97891b9ebaceb29089c677f89
ogin503 <alle.sollen.mir.folgen.1378@gmail.com>
2012-11-30 21:14:52 +0900

    add -sb option

--- set-env.sh
+++ set-env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git config --global alias.co 'checkout'
-git config --global alias.st 'status'
+git config --global alias.st 'status -sb'
 git config --global alias.cm 'commit -a'
 git config --global alias.di 'diff'
 git config --global alias.br 'branch'
