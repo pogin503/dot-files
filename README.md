6ca9d4d13252bcb16775f7f2797c146ba7992a28
pogin503 <pogin503@gmail.com>
2014-08-03 22:19:24 +0900

    bootstarp.shでセットアップするようになった

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # This is my README
+
+# インストール
+
+```
+git clone https://github.com/pogin503/dot-files.git ~/dotfiles
+```
+
+スクリプトを実行してdotfilesを配置する。
+
+```
+./bootstrap.sh
+```
---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/Brewfile ~/.Brewfile
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitconfig_global ~/.gitconfig_global
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.gdbinit ~/.gdbinit
+ln -s ~/dotfiles/.ghci ~/.ghci
+ln -s ~/dotfiles/.gemrc ~/.gemrc
