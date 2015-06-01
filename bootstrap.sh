0e63e4372efa008288edd3954b22105befc6171f
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-06-02 00:39:41 +0900

    .aliasesのシンボリックリンクの処理と、git コマンドのエイリアスの処理を追加
    gitコマンドのエイリアスはset-env.shからもってきた

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/Brewfile ~/Brewfile
@@ -10,3 +11,10 @@ ln -s ~/dotfiles/.gdbinit ~/.gdbinit
 ln -s ~/dotfiles/.ghci ~/.ghci
 ln -s ~/dotfiles/.gemrc ~/.gemrc
 ln -s ~/dotfiles/.guile ~/.guile
+ln -s ~/dotfiles/.aliases ~/.aliases
+
+git config --global alias.co 'checkout'
+git config --global alias.st 'status -sb'
+git config --global alias.cm 'commit -a'
+git config --global alias.di 'diff'
+git config --global alias.br 'branch'
