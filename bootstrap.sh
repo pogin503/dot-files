962bf2cfe0a09ac278eeb3bd7f7c696fcc3be7c4
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-06-15 07:33:58 +0900

    Fix wrong setting

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,10 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/Brewfile ~/Brewfile
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitconfig_global ~/.gitconfig_global
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.gdbinit ~/.gdbinit
 ln -s ~/dotfiles/.ghci ~/.ghci
-ln -s ~/dotfiles/.gemrc ~/.gemrc
 ln -s ~/dotfiles/.guile ~/.guile
 ln -s ~/dotfiles/.aliases ~/.aliases
 
