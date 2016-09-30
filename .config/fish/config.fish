25c7a673a44bf685d68acb5b0124757c7fd5d21a
pogin <pogin503@gmail.com>
2016-09-30 12:57:02 +0900

    Update rbenv path config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,13 +2,8 @@ set fisher_home ~/workspace/fisherman
 set fisher_config ~/.config/fisherman
 source $fisher_home/config.fish
 
-# export PATH=~/.rbenv/bin:$PATH
-#=> set -x PATH ~/.rbenv/bin $PATH
-
-# which rbenv > /dev/null 2>&1
-# if [ $? -eq 0 ]; then # コマンドが存在すれば
-#     # eval "$(rbenv init -)"
-#     set -x PATH $HOME/.rbenv/bin $PATH
-#     set -x PATH $HOME/.rbenv/shims $PATH
-#     rbenv rehash >/dev/null ~&1
-# fi
+set -x PATH $HOME/.rbenv/bin $PATH
+which rbenv > /dev/null 2>&1
+if [ $status = 0 ] # コマンドが存在すれば
+    status --is-interactive; and . (rbenv init -|psub)
+end
