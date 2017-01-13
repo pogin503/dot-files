c60a6f3778ef00d44ade228d25c06bb5df986705
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2017-01-13 23:02:01 +0900

    Add Brewfile config

--- setup.sh
+++ setup.sh
@@ -33,3 +33,17 @@ wget https://raw.github.com/technomancy/leiningen/stable/bin/lein
 chmod a+x lein
 ./lein
 popd
+
+# Install Homebrew
+if ! type "brew" > /dev/null; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Install Apps
+brew tap Homebrew/bundle
+brew bundle --file=~/dotfiles/Brewfile
+brew linkapps
+brew cleanup
+brew cask cleanup
+brew doctor
+brew cask doctor
