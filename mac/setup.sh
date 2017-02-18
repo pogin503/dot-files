0b499e012576bad90b164f7e3669e3fe98279b86
pogin503 <pogin503@gmail.com>
2017-02-18 20:50:12 +0900

    Add setup.sh for Mac

---
+++ mac/setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Install Homebrew
+if ! type "brew" > /dev/null; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Install Apps
+brew tap Homebrew/bundle
+brew bundle --file=~/dotfiles/Brewfile
+brew linkapps
+brew cleanup
+brew cask cleanup
+brew doctor
+brew cask doctor
--- setup.sh
+++ setup.sh
@@ -31,16 +31,5 @@ chmod a+x lein
 ./lein
 popd
 
-# Install Homebrew
-if ! type "brew" > /dev/null; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-# Install Apps
-brew tap Homebrew/bundle
-brew bundle --file=~/dotfiles/Brewfile
-brew linkapps
-brew cleanup
-brew cask cleanup
-brew doctor
-brew cask doctor
+./rust/setup.sh
+./mac/setup.sh
