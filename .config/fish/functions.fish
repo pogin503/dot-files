ed7035407b97d42b35f56f55c972a0a755cae90e
pogin503 <pogin503@gmail.com>
2017-04-10 23:02:59 +0900

    Add git_template function

--- .config/fish/functions.fish
+++ .config/fish/functions.fish
@@ -7,6 +7,23 @@ function sh_template
   chmod 744 $argv[1]
 end
 
+function git_template
+  printf "$argv[1]\n" > README.md
+  set len (string length $argv[1])
+  ruby -e "$len.times {|i| print '=' }; puts " >> README.md
+  printf "\
+
+#Description
+
+#Requirement
+
+#Usage
+
+#Installation
+
+" | cat >> README.md
+end
+
 function kill_process
   kill -9 (pgrep $argv[1])
 end
