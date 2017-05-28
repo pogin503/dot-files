7675e6bb5a10c0b276f389b351deed055a624d0d
pogin503 <pogin503@gmail.com>
2017-05-28 20:05:46 +0900

    Improve make-role.sh
    - Add “——-” to default main.yml
    - Add comment
    - Add file and meta direcotry

--- makerole.sh
+++ makerole.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
 
-# read 
+# make role directory
 mkdir -p ./roles/"$1"/tasks
 mkdir -p ./roles/"$1"/defaults
 mkdir -p ./roles/"$1"/vars
+# mkdir -p ./roles/"$1"/files
 mkdir -p ./roles/"$1"/templates
 mkdir -p ./roles/"$1"/handlers
+# mkdir -p ./roles/"$1"/meta
 
-touch ./roles/"$1"/tasks/main.yml
-touch ./roles/"$1"/defaults/main.yml
-touch ./roles/"$1"/vars/main.yml
-touch ./roles/"$1"/templates/file.j2
-touch ./roles/"$1"/handlers/main.yml
+# 実行するタスクを定義する
+touch ./roles/"$1"/tasks/main.yml    && printf "---\n" >> ./roles/"$1"/tasks/main.yml
+
+# Role内で使う「外から書き換えるつもり」の変数のデフォルト値を設定する 
+touch ./roles/"$1"/defaults/main.yml && printf "---\n" >> ./roles/"$1"/defaults/main.yml
+
+# Role内で使う「外から書き換えるつもりの***ない***」の変数のデフォルト値を設定する 
+touch ./roles/"$1"/vars/main.yml     && printf "---\n" >> ./roles/"$1"/vars/main.yml
+
+# copyモジュールでリモートに配置するファイルを置くディレクトリ
+# touch ./roles/"$1"/templates/file.j2 && printf "---\n" >> ./roles/"$1"/templates/main.yml
+
+# templateモジュールでリモートに展開するJinja2テンプレートを置くディレクトリ
+touch ./roles/"$1"/templates/file.j2 && printf "---\n" >> ./roles/"$1"/templates/main.yml
+
+# ハンドラーを定義する
+touch ./roles/"$1"/handlers/main.yml && printf "---\n" >> ./roles/"$1"/handlers/main.yml
+
+# Roleの依存関係を記述するファイル
+# touch ./roles/"$1"/handlers/main.yml && printf "---\n" >> ./roles/"$1"/handlers/main.yml
+
