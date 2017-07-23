af2fe9811d73df6d72e8b346e808f142260e3bc8
pogin503 <pogin503@gmail.com>
2017-07-24 03:10:47 +0900

    Update gen_gitignore.sh

--- gen_gitignore.sh
+++ gen_gitignore.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+gibo -u
 gibo Windows macOS Linux C ELisp Haskell Scala Rust Python Emacs Vim Ruby tags Vagrant Xcode JetBrains | uniq > ~/dotfiles/.gitignore_global
-cat >> ./.gitignore_global <<EOF
+cat >> ~/dotfiles/.gitignore_global <<EOF
 .ipynb_checkpoints
 EOF
