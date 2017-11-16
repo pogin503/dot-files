0edee7172cb3d189265a0e80a62600801cc93479
pogin503 <pogin503@gmail.com>
2017-11-16 23:57:53 +0900

    Remove unnecessary code

--- .config/fish/functions.fish
+++ .config/fish/functions.fish
@@ -57,12 +57,10 @@ function ansible_check
   if [ (string length "$argv.length") = 0 ]
     exit
   else if [ (string length "$argv.length") = 1 ]
-    echo 0
     ansible-playbook --syntax-check $argv[1]
     and ansible-playbook --check $argv[1] --vault-password-file ~/.ansible/.vault_password
     and ansible-lint $argv[1]
   else
-    echo 2
     ansible-playbook --syntax-check $argv[1] -i $argv[2]
     and ansible-playbook --check $argv[1] -i $argv[2] --vault-password-file ~/.ansible/.vault_password
     and ansible-lint $argv[1]
