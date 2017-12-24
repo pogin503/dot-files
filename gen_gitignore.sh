3f49575b3fa22a3646b383461e4cdd13f40c0b97
pogin503 <pogin503@gmail.com>
2017-12-24 22:28:45 +0900

    Update gen_gitignore.sh

--- gen_gitignore.sh
+++ gen_gitignore.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
+
+DOT_DIR=~/dotfiles
+
 gibo -u
-gibo Ansible Eclipse Windows macOS Linux C ELisp Haskell Scala Rust Python Emacs Vim Ruby tags Vagrant Xcode JetBrains | uniq > ~/dotfiles/.gitignore_global
+gibo Ansible Eclipse Windows macOS Linux Emacs Vim tags Vagrant Xcode JetBrains | uniq > "$DOT_DIR"/.gitignore_global
+gibo ELisp Haskell Scala Rust Ruby Python Java C | uniq >> "$DOT_DIR"/.gitignore_global
 
-# gibo Ansible Eclipse Windows macOS Linux Emacs Vim tags Vagrant Xcode JetBrains | uniq > ~/dotfiles/.gitignore_global
-# gibo ELisp Haskell Scala Rust Ruby Python Java C | uniq >> ~/dotfiles/.gitignore_global
-sed -i -e s/^\*\.d$// ~/dotfiles/.gitignore_global
+sed -i -e s/^\*\.d$// "$DOT_DIR"/.gitignore_global
 
-cat >> ~/dotfiles/.gitignore_global <<EOF
+cat >> "$DOT_DIR"/.gitignore_global <<EOF
 .ipynb_checkpoints
 EOF
