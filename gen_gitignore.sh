18d86b78e142ed21782a42eed0acc103615026e7
pogin503 <pogin503@gmail.com>
2017-12-24 22:20:43 +0900

    Update .gitignore and .gitignore_global

--- gen_gitignore.sh
+++ gen_gitignore.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -eux
 gibo -u
-gibo Windows macOS Linux C ELisp Haskell Scala Rust Python Emacs Vim Ruby tags Vagrant Xcode JetBrains | uniq > ~/dotfiles/.gitignore_global
+gibo Ansible Eclipse Windows macOS Linux C ELisp Haskell Scala Rust Python Emacs Vim Ruby tags Vagrant Xcode JetBrains | uniq > ~/dotfiles/.gitignore_global
 
+# gibo Ansible Eclipse Windows macOS Linux Emacs Vim tags Vagrant Xcode JetBrains | uniq > ~/dotfiles/.gitignore_global
+# gibo ELisp Haskell Scala Rust Ruby Python Java C | uniq >> ~/dotfiles/.gitignore_global
 sed -i -e s/^\*\.d$// ~/dotfiles/.gitignore_global
 
 cat >> ~/dotfiles/.gitignore_global <<EOF
