3f4bf55a7bee7c234d09b6bc2316f8516c3316a4
pogin503 <pogin503@gmail.com>
2018-04-09 23:29:55 +0900

    setting(tool): Move git files to .config/git
    - Add .commit_template
    - .gitignore_global -> .config/git/.gitignore_global
    - Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,6 @@ fi
 ln -sf "$DOT_DIR"/.bashrc           ~/.bashrc
 ln -sf "$DOT_DIR"/.gdbinit          ~/.gdbinit
 ln -sf "$DOT_DIR"/.ghci             ~/.ghci
-ln -sf "$DOT_DIR"/.gitignore_global ~/.gitignore_global
 ln -sf "$DOT_DIR"/.guile            ~/.guile
 ln -sf "$DOT_DIR"/.rubocop.yml      ~/.rubocop.yml
 ln -sf "$DOT_DIR"/.tmux.conf        ~/.tmux.conf
@@ -20,9 +19,13 @@ ln -sf "$DOT_DIR"/.zshrc            ~/.zshrc
 
 mkdir -p ~/.config/fish
 mkdir -p ~/.config/fish/functions
-ln -sf "$DOT_DIR"/.config/fish/aliases.fish     ~/.config/fish/aliases.fish
-ln -sf "$DOT_DIR"/.config/fish/config.fish      ~/.config/fish/config.fish
+ln -sf "$DOT_DIR"/.config/fish/aliases.fish               ~/.config/fish/aliases.fish
+ln -sf "$DOT_DIR"/.config/fish/config.fish                ~/.config/fish/config.fish
 ln -sf "$DOT_DIR"/.config/fish/functions/functions.fish   ~/.config/fish/functions/functions.fish
+
+mkdir -p ~/.config/git
+ln -sf "$DOT_DIR"/.config/git/.commit_template  ~/.config/git/.commit_template
+ln -sf "$DOT_DIR"/.config/git/.gitignore_global ~/.config/git/.gitignore_global
 
 mkdir -p ~/.stack/
 ln -sf "$DOT_DIR"/.stack/config.yaml ~/.stack/config.yaml
@@ -36,9 +39,9 @@ git config --global alias.br 'branch'
 git config --global user.name  "pogin503"
 git config --global user.email "pogin503@gmail.com"
 
-git config --global core.excludesfile ~/.gitignore_global
+git config --global core.excludesfile ~/.config/git/.gitignore_global
 # git config --global core.editor emacsclient
-
+git config --global commit.template   ~/.config/git/.commit_template
 git config --global core.quotepath false
 
 # "$DOT_DIR"/clojure/setup.sh
