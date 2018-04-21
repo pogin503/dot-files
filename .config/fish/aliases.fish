14bec55db0338ddd19a35f895e503373c5f0d33c
pogin503 <pogin503@gmail.com>
2018-04-21 17:57:44 +0900

    setting(alias): Update google chrome alias

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -64,7 +64,9 @@ if not [ "$USER" = "root" ]
 end
 
 if [ (uname) = Darwin ]
-    alias chrome='open -a "Google Chrome"'
+    alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+    alias chrome-canary="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
+    alias chromium="/Applications/Chromium.app/Contents/MacOS/Chromium"
     alias firefox='open -a Firefox'
     alias safari='open -a Safari'
     alias itunes='open -a iTunes'
