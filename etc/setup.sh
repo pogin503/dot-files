e9bc6e98e0f8a8443ef0b412d436be6e0f82cd63
pogin503 <pogin503@gmail.com>
2019-11-09 17:08:49 +0900

    fix: fix error

--- etc/setup.sh
+++ etc/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(dirname "$0")
-"$SCRIPT_DIR"/env.sh
+. "$SCRIPT_DIR"/env.sh
 
 if [ "$(uname)" = 'Darwin' ];then
   ln -sf "$DOT_DIR"/etc/launchd/com.pogin.jupyterlab.plist ~/Library/LaunchAgents/com.pogin.jupyterlab.plist
