4d82bd88d9c64c6debb4cece043faf98a9d8c420
pogin503 <pogin503@gmail.com>
2020-05-13 01:47:21 +0900

    feat: add exec-role.sh

---
+++ bin/exec-role.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+PROGNAME=$(basename "$0")
+if [[ $# -lt 2 ]]; then
+    cat <<HELP
+Wrapper script for ansible-playbook to apply single role.
+
+Usage: ./bin/$PROGNAME <host-pattern> <role-name> [ansible-playbook options]
+
+Examples:
+  ./bin/$PROGNAME dest_host my_role
+  ./bin/$PROGNAME custom_host my_role -i 'custom_host,' -vv --check
+HELP
+    exit
+fi
+
+HOST_PATTERN=$1
+shift
+ROLE=$1
+shift
+
+echo "Trying to apply role \"$ROLE\" to host/group \"$HOST_PATTERN\"..."
+ANSIBLE_ROLES_PATH="$(pwd)/roles:$HOME/.ansible/roles"
+ANSIBLE_CONFIG="$HOME"/.ansible.cfg
+export ANSIBLE_ROLES_PATH
+export ANSIBLE_CONFIG
+echo "${ANSIBLE_ROLES_PATH}"
+export ANSIBLE_RETRY_FILES_ENABLED="False"
+# connection: localhost
+# gather_facts: False
+YAML_DATA=$(cat << END
+---
+- hosts: $HOST_PATTERN
+  pre_tasks:
+    - debug: msg={{ ansible_env.PWD }}
+  roles:
+    - $ROLE
+END
+            )
+echo $YAML_DATA
+ansible-playbook "$@" /dev/stdin <<END
+---
+- hosts: $HOST_PATTERN
+  pre_tasks:
+    - debug: msg={{ansible_env.PWD}}
+  roles:
+    - $ROLE
+END
+
+# @see https://stackoverflow.com/questions/38350674/ansible-can-i-execute-role-from-command-line
