994ec75d8a3e159121a6009534c51369c831c0a2
pogin503 <pogin503@gmail.com>
2020-06-28 03:17:52 +0900

    feat: set container name

--- docker/my-dev/docker-run.sh
+++ docker/my-dev/docker-run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export DOCKER_BUILDKIT=1
 docker build . --secret id=ssh,src=$HOME/.ssh/id_rsa -t my-dev && \
-  docker run -d -it my-dev
+  docker run -d -it my-dev --name my-dev
