ae4dfd7588f22db00567d0fdc26c29a57151a695
pogin503 <pogin503@gmail.com>
2021-03-21 17:47:51 +0900

    refactor: add etc/fish_path.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,9 +14,6 @@ end
 # load functions
 . "$HOME"/.config/fish/functions/functions.fish
 
-# set -x XDG_CONFIG_HOME "$HOME/.config"
-# set -x XDG_CACHE_HOME "$HOME/.cache"
-# set -x XDG_DATA_HOME "$HOME/.local/share"
 # set -x XDG_RUNTIME_DIR
 # set -x XDG_DATA_DIRS "$HOME/.local/share"
 # if [[ (uname) = 'Darwin' ]]; then]
---
+++ etc/fish_path.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+set -U XDG_CONFIG_HOME "$HOME/.config"
+set -U XDG_CACHE_HOME "$HOME/.cache"
+set -U XDG_DATA_HOME "$HOME/.local/share"
