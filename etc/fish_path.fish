721c6dda99fd7de95173a014ebf3f5af9080c3b9
pogin503 <pogin503@gmail.com>
2021-08-14 19:34:33 +0900

    feat: update path setting

--- etc/fish_path.fish
+++ etc/fish_path.fish
@@ -1,5 +1,8 @@
 #!/usr/bin/env fish
 
-set -U XDG_CONFIG_HOME "$HOME/.config"
-set -U XDG_CACHE_HOME "$HOME/.cache"
-set -U XDG_DATA_HOME "$HOME/.local/share"
+set -Ux XDG_CONFIG_HOME "$HOME/.config"
+set -Ux XDG_CACHE_HOME "$HOME/.cache"
+set -Ux XDG_DATA_HOME "$HOME/.local/share"
+set -Ux XDG_STATE_HOME "$HOME/.local/state"
+set -Ux HOMEBREW_CACHE /Volumes/ext_SSD/.cache/Homebrew
+set -Ux HOMEBREW_LOGS "$HOMEBREW_CACHE/Logs"
