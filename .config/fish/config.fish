d0ab4db2000441bcb115d42c5cf467284393a55e
pogin503 <pogin503@gmail.com>
2021-12-25 01:59:06 +0900

    refactor: move path setting to etc/fish_path.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -142,64 +142,17 @@ end
 set -x RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src
 set -x RUSTC_WRAPPER (which sccache)
 
-# ruby
-# set -x GEM_HOME "$XDG_DATA_HOME"/gem
-# set -x PATH "$GEM_HOME"/bin $PATH
-# set -x GEM_SPEC_CACHE "$XDG_CACHE_HOME"/gem
-set -x BUNDLE_USER_CONFIG "$XDG_CONFIG_HOME"/bundle
-set -x BUNDLE_USER_CACHE "$XDG_CACHE_HOME"/bundle
-set -x BUNDLE_USER_PLUGIN "$XDG_DATA_HOME"/bundle
-# set -x IRBRC "$XDG_CONFIG_HOME"/irb/irbrc
-
-# set -x HISTFILE "$XDG_DATA_HOME"/bash/history
-set -x JUPYTER_CONFIG_DIR "$XDG_CONFIG_HOME"/jupyter
-set -x JUPYTER_DATA_DIR "$XDG_DATA_HOME"/jupyter
-
-set -x VAGRANT_ALIAS_FILE "$XDG_DATA_HOME"/vagrant/aliases
-set -x VAGRANT_HOME "$XDG_DATA_HOME"/vagrant
-set -x DOCKER_CONFIG "$XDG_CONFIG_HOME"/docker
-set -x MACHINE_STORAGE_PATH "$XDG_DATA_HOME"/docker-machine
-
-set -x MYSQL_HISTFILE "$XDG_DATA_HOME"/mysql_history
-
-set -x PSQLRC "$XDG_CONFIG_HOME/pg/psqlrc"
-set -x PSQL_HISTORY "$XDG_CACHE_HOME/pg/psql_history"
-set -x PGPASSFILE "$XDG_CONFIG_HOME/pg/pgpass"
-set -x PGSERVICEFILE "$XDG_CONFIG_HOME/pg/pg_service.conf"
-
-set -x SQLITE_HISTORY "$XDG_DATA_HOME"/sqlite_history
-
-set -x OCTAVE_HISTFILE "$XDG_CACHE_HOME/octave-hsts"
-set -x OCTAVE_SITE_INITFILE "$XDG_CONFIG_HOME/octave/octaverc"
-
-set -x AWS_SHARED_CREDENTIALS_FILE "$XDG_CONFIG_HOME"/aws/credentials
-set -x AWS_CONFIG_FILE "$XDG_CONFIG_HOME"/aws/config
-
 set -x GNUPGHOME "$XDG_DATA_HOME"/gnupg
 if type -q gpg2
   gpg2 --homedir "$XDG_DATA_HOME"/gnupg
 end
 
 mkdir -p "$XDG_CACHE_HOME"/less
-set -x LESSKEY "$XDG_CONFIG_HOME"/less/lesskey
-set -x LESSHISTFILE "$XDG_CACHE_HOME"/less/lesshst
 
-set -x GRADLE_USER_HOME "$XDG_DATA_HOME"/gradle
 
-# set -x XINITRC "$XDG_CONFIG_HOME"/X11/xinitrcx
-# set -x XSERVERRC "$XDG_CONFIG_HOME"/X11/xserverrc
-
-set -x OCTAVE_HISTFILE "$XDG_CACHE_HOME/octave-hsts"a
-set -x OCTAVE_SITE_INITFILE "$XDG_CONFIG_HOME/octave/octaverc"
-set -x PYTHON_EGG_CACHE "$XDG_CACHE_HOME"/python-eggs
-set -x PYLINTHOME "$XDG_CACHE_HOME"/pylint
-set -x INPUTRC "$XDG_CONFIG_HOME"/readline/inputrc
-set -x ANSIBLE_INVENTORY "$HOME"/ansible_hosts
-set -x ANSIBLE_CONFIG ~/.config/ansible/ansible.cfg
 # direnv setting
 eval (direnv hook fish)
 set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
 
 # Wasmer
-export WASMER_DIR="/Users/iMac/.wasmer"
 [ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
--- etc/fish_path.fish
+++ etc/fish_path.fish
@@ -6,3 +6,54 @@ set -Ux XDG_DATA_HOME "$HOME/.local/share"
 set -Ux XDG_STATE_HOME "$HOME/.local/state"
 set -Ux HOMEBREW_CACHE /Volumes/ext_SSD/.cache/Homebrew
 set -Ux HOMEBREW_LOGS "$HOMEBREW_CACHE/Logs"
+
+# ruby
+# set -x GEM_HOME "$XDG_DATA_HOME"/gem
+# set -x PATH "$GEM_HOME"/bin $PATH
+# set -x GEM_SPEC_CACHE "$XDG_CACHE_HOME"/gem
+set -Ux BUNDLE_USER_CONFIG "$XDG_CONFIG_HOME"/bundle
+set -Ux BUNDLE_USER_CACHE "$XDG_CACHE_HOME"/bundle
+set -Ux BUNDLE_USER_PLUGIN "$XDG_DATA_HOME"/bundle
+# set -x IRBRC "$XDG_CONFIG_HOME"/irb/irbrc
+
+# set -x HISTFILE "$XDG_DATA_HOME"/bash/history
+set -Ux JUPYTER_CONFIG_DIR "$XDG_CONFIG_HOME"/jupyter
+set -Ux JUPYTER_DATA_DIR "$XDG_DATA_HOME"/jupyter
+
+set -Ux VAGRANT_ALIAS_FILE "$XDG_DATA_HOME"/vagrant/aliases
+set -Ux VAGRANT_HOME "$XDG_DATA_HOME"/vagrant
+set -Ux DOCKER_CONFIG "$XDG_CONFIG_HOME"/docker
+set -Ux MACHINE_STORAGE_PATH "$XDG_DATA_HOME"/docker-machine
+
+set -Ux MYSQL_HISTFILE "$XDG_DATA_HOME"/mysql_history
+
+set -Ux PSQLRC "$XDG_CONFIG_HOME/pg/psqlrc"
+set -Ux PSQL_HISTORY "$XDG_CACHE_HOME/pg/psql_history"
+set -Ux PGPASSFILE "$XDG_CONFIG_HOME/pg/pgpass"
+set -Ux PGSERVICEFILE "$XDG_CONFIG_HOME/pg/pg_service.conf"
+
+set -Ux SQLITE_HISTORY "$XDG_DATA_HOME"/sqlite_history
+
+set -Ux OCTAVE_HISTFILE "$XDG_CACHE_HOME/octave-hsts"
+set -Ux OCTAVE_SITE_INITFILE "$XDG_CONFIG_HOME/octave/octaverc"
+
+set -Ux AWS_SHARED_CREDENTIALS_FILE "$XDG_CONFIG_HOME"/aws/credentials
+set -Ux AWS_CONFIG_FILE "$XDG_CONFIG_HOME"/aws/config
+
+set -Ux LESSKEY "$XDG_CONFIG_HOME"/less/lesskey
+set -Ux LESSHISTFILE "$XDG_CACHE_HOME"/less/lesshst
+
+set -Ux GRADLE_USER_HOME "$XDG_DATA_HOME"/gradle
+
+# set -Ux XINITRC "$XDG_CONFIG_HOME"/X11/xinitrcx
+# set -Ux XSERVERRC "$XDG_CONFIG_HOME"/X11/xserverrc
+
+set -Ux OCTAVE_HISTFILE "$XDG_CACHE_HOME/octave-hsts"a
+set -Ux OCTAVE_SITE_INITFILE "$XDG_CONFIG_HOME/octave/octaverc"
+set -Ux PYTHON_EGG_CACHE "$XDG_CACHE_HOME"/python-eggs
+set -Ux PYLINTHOME "$XDG_CACHE_HOME"/pylint
+set -Ux INPUTRC "$XDG_CONFIG_HOME"/readline/inputrc
+set -Ux ANSIBLE_INVENTORY "$HOME"/ansible_hosts
+set -Ux ANSIBLE_CONFIG "$XDG_CONFIG_HOME"/ansible/ansible.cfg
+
+set -Ux WASMER_DIR="$HOME/.wasmer"
