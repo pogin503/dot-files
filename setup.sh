53475363d7a7f46e3814d720f75ee8c22dced601
pogin503 <pogin503@gmail.com>
2023-03-05 02:13:26 +0900

    feat: update git status alias

--- setup.sh
+++ setup.sh
@@ -47,7 +47,7 @@ ln -sf "$DOT_DIR"/.stack/config.yaml ~/.stack/config.yaml
 
 # git config
 git config --global alias.co 'checkout'
-git config --global alias.st 'status -sb'
+git config --global alias.st 'status -s'
 git config --global alias.cm 'commit -a'
 git config --global alias.di 'diff'
 git config --global alias.br 'branch'
